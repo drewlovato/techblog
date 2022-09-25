2be76466354bec47040a69817f21fca125ff75b8
Drew Lovato <gaethlovato@gmail.com>
2022-09-25 15:22:53 -0600

    added comment model

---
+++ models/Comment.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    comment_details: {
+        type: DataTypes.STRING,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references:{
+            model: 'user',
+            key: 'id',
+        },
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references:{
+            model: 'post',
+            key: 'id',
+        },
+    },
+},
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
+    }
+);
