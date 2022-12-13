14fbc2f09942edbe4c4c720ce12260bc51736cc4
Drew Lovato <gaethlovato@gmail.com>
2022-12-13 07:10:18 -0700

    updating files

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -4,12 +4,14 @@ const formHandler = async (event) => {
   const postId = document.querySelector("#labelPostId").textContent.trim();
   const title = document.querySelector("#textTitle").value.trim();
   const content = document.querySelector("#textContent").value.trim();
+  const dateCreated = document.querySelector("#textDateCreated").value.trim();
 
-  if (postId && title && content) {
+  if (postId && title && content && dateCreated) {
     const response = await fetch(`/api/posts/${postId}`, {
       method: "PUT",
       body: JSON.stringify({
         title,
+        dateCreated,
         content,
       }),
       headers: { "Content-Type": "application/json" },
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,14 @@
 module.exports = {
+  format_time: (date) => {
+    return date.toLocaleTimeString();
+  },
   format_date: (date) => {
     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  },
+  format_datetime: (date) => {
+    return new Date(date).toLocaleString();
+  },
+  trim_string: (value) => {
+    return value.trim();
   },
 };
